187bc90c09c4bc0e02871311d84647752cab8478
Snxhit <amitdgames@gmail.com>
2026-08-31 01:01:19 +0530

    setup VCS engine struct

--- snehit_deshmukh/src/main.rs
+++ snehit_deshmukh/src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+use std::fs;
+use std::path::{Path, PathBuf};
+
+struct VCSEngine {
+    dir: PathBuf,
+    snapshots: PathBuf,
+    logs: PathBuf,
+}
+
+impl VCSEngine {
+    fn new() -> Self {
+        let current_dir = env::current_dir().expect("Failed to get current dir.");
+        VCSEngine {
+            dir: current_dir.join(".vcs"),
+            snapshots: current_dir.join(".vcs/snapshots"),
+            logs: current_dir.join(".vcs/log.txt"),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    todo!();
 }
